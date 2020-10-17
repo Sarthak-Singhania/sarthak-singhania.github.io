0f547515144dc6bcc23c56b7a2373a723c7f292c
sarthak2806 <sathak2806singhania@gmail.com>
2020-10-17 21:41:01 +0530

    new commit

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -9,40 +9,36 @@ function clicker(headingText) {
     document.querySelector(`aside > a.${headingText.toLowerCase()}`).classList.add("selected")
 
     let heading = document.querySelector('#newsList > h2');
-    heading.innerText = headingText + ' Images';
+    heading.innerText = headingText + ' Photos';
     category = headingText.toLowerCase();
     fetcher();
 }
 
 async function fetcher()
 {
-    document.querySelector("#topStoriesContainer").innerHTML = "";
+    document.querySelector("#imageList").innerHTML = "";
 
     const resp = await fetch(`https://api.unsplash.com/search/photos/?per_page=20&query=${category}&client_id=algpuhSHqEqgZxXRQxk7XfTaQLrUJDebVj7nO-8fR40`);
-    const ids = await resp.json();
-    //JSON = JavaScript Object Notation
-    //const itemsArray = [];
-    console.log(ids);
-    // for(let i = 0 ; i < 20 ; i++)
-    // {
-    //     const itemData = await fetch(`https://hacker-news.firebaseio.com/v0/item/${items[i]}.json`);
-    //     const resp = await itemData.json();
-    //     itemsArray.push(resp);
+    const resp_ids = await resp.json();
+    const ids = resp_ids.results
+    for(let i = 0 ; i < 20 ; i++)
+    {
+        const resp=ids[i];
+        let description;
+        if(resp.alt_description==null) 
+        {
+            description="Nothing to describe";
+        }
+        else
+        {
+            description=resp.alt_description
+        }
+        const inserter = 
+        `<div><p style="font-size: 0.8rem">${i+1}) ${description}</p></div>` + 
+        `<div><img src="${resp.urls.full}" width="200" style="margin: 1rem; border-radius: 5px;"></img></div>`;
 
-    //     const inserter = 
-    //     `<a class="news-item" href="${resp.url}" target="blank" data-title="${resp.title}">` +
-    //         `<h3 class="news-title">${resp.title}</h3>` +
-    //         `<p class="news-byline">${resp.by}</p>` +
-    //         `<p class="news-time">${resp.time}</p>` +
-    //         `<p class="news-score">` +
-    //             `<i class="fa fa-thumbs-up"></i>${resp.score}` +
-    //         `</p>` +
-    //     `</a>`;
-
-    //     console.log(inserter)
-
-    //     document.querySelector("#topStoriesContainer").insertAdjacentHTML("beforeend", inserter);
-    // }
+        document.querySelector("#imageList").insertAdjacentHTML("beforeend", inserter);
+    }
 }
 
 function main()
@@ -68,6 +64,3 @@ function sports(){
 function nature(){
     clicker("Nature");
 }
-
-// useTimeout and useInterval
-// blocking and non-blocking
